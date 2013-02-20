1e28207756708f8c852321af1f6737f0692470b9
Matthew Batchelder <borkweb@gmail.com>
2013-02-20 14:15:05 -0500

    Changing the init add_action weight

--- vip-loader.php
+++ vip-loader.php
@@ -31,7 +31,7 @@ require_once __DIR__ . '/components/class-go-opencalais-enrich.php';
 require_once __DIR__ . '/components/class-go-opencalais-autotagger.php';
 require_once __DIR__ . '/components/functions.php';
 
-add_action( 'init', 'go_oc_extras_init' );
+add_action( 'init', 'go_oc_extras_init', 1 );
 
 $go_opencalais = new GO_OpenCalais;
 $go_opencalais_autotagger = new GO_OpenCalais_AutoTagger;
