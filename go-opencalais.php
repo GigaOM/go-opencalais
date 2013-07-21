55f843b13776725bb5282d8ace755d51801afeab
Casey Bisson <casey.bisson@gmail.com>
2013-07-21 11:30:38 -0700

    Remove taxonomy registration
    This should be handled in https://github.com/GigaOM/legacy-pro/tree/master/application/content/plugins/go-taxonomy
    
    With config files here:
    
    https://github.com/GigaOM/gigaom/blob/master/config/_search/go-taxonomy.php
    
    http://github.com/GigaOM/legacy-pro/blob/master/application/content/themes/gigaom-pro/config/_pro/go-taxonomy.php
    
    Additional config files are needed for GO/pC, and the plugin needs to be submoduled in gigaom-plugins

--- go-opencalais.php
+++ go-opencalais.php
@@ -13,9 +13,6 @@ After upgrade/install, please run: /wp-admin/admin-ajax.php?action=oc_autotag_up
 require_once __DIR__ . '/components/class-go-opencalais.php';
 require_once __DIR__ . '/components/class-go-opencalais-enrich.php';
 require_once __DIR__ . '/components/class-go-opencalais-autotagger.php';
-require_once __DIR__ . '/components/functions.php';
-
-add_action( 'init', 'go_oc_extras_init', 1 );
 
 go_opencalais();
 new GO_OpenCalais_AutoTagger;
