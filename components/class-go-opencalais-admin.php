87318eae1c8bb05ea8e08086b86cbcd95f3616eb
Casey Bisson <casey.bisson@gmail.com>
2013-07-22 17:20:29 -0700

    Removed filter as per my dithering in https://github.com/misterbisson/go-opencalais/pull/1

--- components/class-go-opencalais-admin.php
+++ components/class-go-opencalais-admin.php
@@ -48,10 +48,6 @@ class GO_OpenCalais_Admin
 		add_filter( 'go_oc_response', array( $this, 'filter_insert_socialtags_as_entities' ), 1 );
 		add_filter( 'go_oc_response', array( $this, 'filter_normalize_relevance' ), 3 );
 		add_filter( 'go_oc_response', array( $this, 'filter_response_threshold' ), 7 );
-		if ( function_exists( 'authority_record' ) )
-		{
-			add_filter( 'go_oc_response', array( $this, 'filter_response_add_terms' ), 7 );
-		}
 
 		// check if the autotagger is configged before loading it
 		if ( is_array( $this->config['autotagger'] ) )
@@ -282,48 +278,6 @@ class GO_OpenCalais_Admin
 		return array_filter( $response, array( $this, '_filter_response_threshold' ) );
 	}//end filter_response_threshold
 
-	public function filter_response_add_terms( $response )
-	{
-		foreach ( $response as $k => $v )
-		{
-			if ( isset( $v->_type, $v->name, $this->config['mapping'][ $v->_type ] ) )
-			{
-				// is there a term for this, um, term?
-				if ( ! $term = get_term_by( 'name', $v->name, $this->config['mapping'][ $v->_type ] ) )
-				{
-					// attempt to create a proper term if none exists
-					$term_id = wp_insert_term( $v->name, $this->config['mapping'][ $v->_type ] );
-					$term = get_term( $term_id['term_id'], $this->config['mapping'][ $v->_type ] );
-				}
-
-				// if we found or create a term object for this suggestion
-				if ( isset( $term->name ) )
-				{
-
-					// check if there's an authority record for this term
-					if( $authority = authority_record()->get_term_authority( $term ) )
-					{
-						// set the suggested name to match the authoritative term name
-						$response[ $k ]->name = $authority->primary_term->name;
-
-						// replace the entity type to match the authority, if we have a mapping
-						if ( $new_type = array_search( $authority->primary_term->taxonomy, $this->config['mapping'] ) )
-						{
-							$response[ $k ]->_type = $new_type;
-						}
-					}
-					else
-					{
-						// even if there is no authoritative term, reset the suggested name to so it matches the proper term
-						$response[ $k ]->name = $term->name;
-					}
-				}
-			}
-		}
-
-		return $response;
-	}//end filter_response_add_terms
-
 	/**
 	 * Predictable path for CSS declarations that reference admin media.
 	 */
@@ -409,7 +363,6 @@ class GO_OpenCalais_Admin
 	// a singleton for the admin object
 	public function new_enrich_obj( $post )
 	{
-
 		// fail if the config isn't set
 		if ( ! isset( $this->config['api_key'], $this->config['mapping'] ) )
 		{
