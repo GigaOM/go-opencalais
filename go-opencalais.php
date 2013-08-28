06d4a7f9cdbe1d107dbb4bbded4733be1917e483
Zach Tirrell <zbtirrell@gmail.com>
2013-08-28 08:51:58 -0400

    updating comment header block

--- go-opencalais.php
+++ go-opencalais.php
@@ -1,14 +1,14 @@
 <?php
-/*
-Plugin Name: GigaOM OpenCalais
-Plugin URI:
-Description:
-Version: 0.1
-Author: Adam Backstrom for GigaOM
-Author URI: http://sixohthree.com/
-License: GPL2
-After upgrade/install, please run: /wp-admin/admin-ajax.php?action=oc_autotag_update
-*/
+/**
+ * Plugin Name: Gigaom OpenCalais
+ * Plugin URI:
+ * Description:
+ * Version: 0.1
+ * Author: Adam Backstrom for Gigaom
+ * Author URI: http://sixohthree.com/
+ * License: GPL2
+ * After upgrade/install, please run: /wp-admin/admin-ajax.php?action=oc_autotag_update
+ */
 
 require_once __DIR__ . '/components/class-go-opencalais.php';
 require_once __DIR__ . '/components/class-go-opencalais-enrich.php';
